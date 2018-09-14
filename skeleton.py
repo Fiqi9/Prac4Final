0cad1e132bfef2eb07cb0fc3b787b34c948ee7cf
ameerconrad <moegamadameerconrad@gmail.com>
2018-09-14 12:15:19 +0200

    Time added

--- skeleton.py
+++ skeleton.py
@@ -29,14 +29,14 @@ GPIO.setup(SPICS, GPIO.OUT)
 
 mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK, cs=SPICS, miso=SPIMISO, mosi=SPIMOSI)
 
-def one(channel):
-def two(channel):
-def three(channel):
+#def one(channel):
+#def two(channel):
+#def three(channel):
 
-while(1):
-    GPIO.add_event_detect(switch1, GPIO.FALLING, callback=one, bouncetime=300)
-    GPIO.add_event_detect(switch2, GPIO.FALLING, callback=two, bouncetime=300)
-    GPIO.add_event_detect(switch3, GPIO.FALLING, callback=three, bouncetime=300)
+#while(1):
+ #   GPIO.add_event_detect(switch1, GPIO.FALLING, callback=one, bouncetime=300)
+  #  GPIO.add_event_detect(switch2, GPIO.FALLING, callback=two, bouncetime=300)
+   # GPIO.add_event_detect(switch3, GPIO.FALLING, callback=three, bouncetime=300)
 
 def ConvertVolts(data,places):
  volts = (data * 3.3) / float(1023)
@@ -45,27 +45,34 @@ def ConvertVolts(data,places):
 
 print('Reading MCP3008 values, press Ctrl-C to quit...')
 # Print nice channel column headers.
-print('| Pot  | Temp   | Light |')
+print('| Time | Timer | Pot  | Temp   | Light |')
 print('-' * 24)
 # Main program loop.
+
 while True:
-    values = [0]*3
+
+    values = [0]*5
     #for i in range(8):
         # The read_adc function will get the value of the specified channel (0-7).
     pot_data = mcp.read_adc(0)
-    values[0] = ConvertVolts(pot_data,2)
+    values[2] = ConvertVolts(pot_data,2)
      
     temperature_data = mcp.read_adc(1) #Temperature
-    values[1] = int(0.3921*temperature_data - 60.891)  #Excel linear approximation
+    values[3] = int(0.3921*temperature_data - 60.891)  #Excel linear approximation
     
     ldr_data = mcp.read_adc(2)
-    values[2] = int(0.11*ldr_data - 2.3102)
+    values[4] = int(0.11*ldr_data - 2.3102)
     
     
+    hour = time.strftime("%H")
+    min = time.strftime("%M")
+    sec = time.strftime("%S")
+    
+    values[0] = hour+":"+min+":"+sec
     
      #values[i] = mcp.read_adc(i)
     # Print the ADC values.
-    print('| {0:>4} | {1:>4} C | {2:>4}% |'.format(*values))
+    print('| {0:>4} | {1:>4} | {2:>4} C | {3:>4}% | {4:>4} |'.format(*values))
     # Pause for half a second.
     time.sleep(0.5)
 
